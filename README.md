146c347e99088cb26cf810bc85ce09776cef7a41
Gan0803 <gan0803@gmail.com>
2021-12-06 15:35:17 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# aterm-resetter
+# これは何？
+
+ルーター（Aterm WG1200HS4）を再起動するスクリプトです。
+
+# どうやって使う？
+
+本スクリプトはpython 3.8.12で動作します。
+
+`pip install -r requirements.txt`で必要なパッケージをインストールしてください。
+
+aterm-resetter.pyと同じディレクトリにsecrets.yamlファイルを作成し、以下の情報を記載してください。
+
+```
+username: "username"
+password: "password"
+url = "http://192.168.10.1/"
+```
---
+++ aterm-resetter.py
@@ -0,0 +1,48 @@
+
+from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome import service as fs
+import time
+from selenium.webdriver.common.by import By
+import urllib
+import re
+import yaml
+
+with open("secrets.yaml") as file:
+	yaml_dict = yaml.safe_load(file)
+	username, password, url = yaml_dict["username"], yaml_dict["password"], yaml_dict["url"]
+
+CHROME_DRIVER = "/usr/bin/chromedriver"
+
+options = Options()
+options.add_argument("--headless")
+
+chrome_service = fs.Service(executable_path=CHROME_DRIVER)
+driver = webdriver.Chrome(service=chrome_service, options=options)
+driver.get(url)
+
+driver.implicitly_wait(5)
+
+username_form = driver.find_element(By.NAME, "user")
+password_form = driver.find_element(By.NAME, "pass")
+
+username_form.send_keys(username)
+password_form.send_keys(password)
+
+driver.implicitly_wait(5)
+driver.find_element(By.LINK_TEXT, "ログイン").click()
+
+driver.implicitly_wait(5)
+driver.find_element(By.LINK_TEXT, "再起動").click()
+
+driver.implicitly_wait(5)
+driver.find_element(By.LINK_TEXT, "再起動").click()
+
+driver.implicitly_wait(5)
+driver.find_element(By.LINK_TEXT, "OK").click()
+
+time.sleep(3)
+
+driver.quit()
